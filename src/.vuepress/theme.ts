286be6e639e2deb070f274cc97346452496c4b02
MojaveHao <HTTcode2020@126.com>
2024-02-20 20:57:27 +0800

    Update theme.ts

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -37,11 +37,11 @@ export default hopeTheme({
   //  },
   //},
 
-  editLink: false,
+  editLink: true,
 
   // 多语言配置
   metaLocales: {
-    //editLink: "在 GitHub 上编辑此页",
+    editLink: "在Github上编辑",
   },
 
   // 如果想要实时查看任何改变，启用它。注: 这对更新性能有很大负面影响
