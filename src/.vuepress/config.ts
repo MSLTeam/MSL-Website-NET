6ac6fe53b057985a9d17100ebaf7fe5448b8f93e
luluxiaoyu <ljy.xiaoyu@qq.com>
2024-04-21 22:50:56 +0800

    feat: 谷歌分析

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -9,7 +9,17 @@ export default defineUserConfig({
   description: "Minecraft Server Launcher(MSL)",
 
   theme,
-
+  head: [
+    ["script", { async: true, src: "https://www.googletagmanager.com/gtag/js?id=G-SR9D9TXKVJ" }],
+    ["script", {}, 
+    `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-SR9D9TXKVJ');
+    `
+    ],
+  ],
   // 和 PWA 一起启用
   // shouldPrefetch: false,
 });
