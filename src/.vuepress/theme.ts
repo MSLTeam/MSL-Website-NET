69d30355ba319431cb5e179e283ae1e22f0e886c
luluxiaoyu <ljy.xiaoyu@qq.com>
2025-04-01 23:19:00 +0800

    BILI组件

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -64,7 +64,7 @@ export default hopeTheme({
     searchPro: true,
 
     components: {
-      components: ["Badge", "VPCard"],
+      components: ["Badge", "VPCard","BiliBili"],
     },
 
     // 此处开启了很多功能用于演示，你应仅保留用到的功能。
