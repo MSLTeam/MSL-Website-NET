ec5b3c313a3384c07eb9192baec0242ad8334f90
GuHanDuRen <wtc2778318425@163.com>
2025-04-13 22:56:50 +0800

    完善网页显示效果

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -8,7 +8,7 @@ export default sidebar({
       icon: "laptop-code",
       prefix: "docs/",
       link: "docs/",
-      children: ["start","server-config","frp","p2p","config","launch_bds"],
+      children: ["start","server-config","frp","p2p","config","launch_bds","faq","neoforge_foge"],
     },
     {
       text: "其它文档",
