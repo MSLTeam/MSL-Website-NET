77e7cdae75151b5fcb373cda9c96e099234f12b8
luluxiaoyu <ljy.xiaoyu@qq.com>
2025-05-24 17:10:34 +0800

    icon

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -127,8 +127,10 @@ export default hopeTheme({
     slimsearch: true,
 
     icon: {
-      assets: ["https://lf26-cdn-tos.bytecdntp.com/cdn/expire-1-M/font-awesome/6.0.0/js/fontawesome.min.js",],
-      prefix: "fas",
+      assets: ["https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/font-awesome/6.0.0/js/brands.min.js",
+        "https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/font-awesome/6.0.0/js/solid.min.js",
+        "https://lf26-cdn-tos.bytecdntp.com/cdn/expire-1-M/font-awesome/6.0.0/js/fontawesome.min.js",],
+      prefix: "fa-brands fa-",
     },
 
 
